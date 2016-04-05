2f639938ca68078a6a7e978e8b61d9b52dad3959
Vivek Vyas <vvkv94@gmail.com>
2016-04-05 00:20:13 -0700

    Added files via upload
    As the code starts running, the orange light is briefly on switching right over to the red light. The red LED stays on for over 7 seconds and then there is under 3 seconds where both red and orange LEDs turn on. Finally, the green LED turns on.
    
    The goal of this project is to get a basic understanding of the coding environment while keeping the challenge level relatively low.

---
+++ TrafficLights.ino
@@ -0,0 +1,40 @@
+int red = 13;
+int yellow = 12;
+int green = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(red,OUTPUT);
+pinMode(yellow,OUTPUT);
+pinMode(green,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+changeLights();
+delay(15000);
+}
+
+void changeLights(){
+  //green off, yellow for 3 seconds
+  digitalWrite(green, LOW);
+  digitalWrite(yellow,HIGH);
+  delay(3000);
+
+  //turn yellow off, then turn red on for 5 seconds
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,HIGH);
+  delay(10000);
+
+  //red and yellow on for 2 seconds (red is already on)
+  digitalWrite(yellow,HIGH);
+  delay(2000);
+
+  //turn off red and yellow and turn green on
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
+}
+
+
+
